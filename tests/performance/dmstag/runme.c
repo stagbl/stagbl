854ea14aa16501149119c1300822bc436d7bfa4b
Patrick Sanan <patrick.sanan@gmail.com>
2018-07-03 07:39:18 +0200

    DMStag test: change to box (since that's what's currently implemented)

--- tests/performance/dmstag/runme.c
+++ tests/performance/dmstag/runme.c
@@ -8,7 +8,12 @@ static char help[] = "Perform some standard operations to compare DMStag and a c
 #include <petscdmda.h>
 #include <petscdmcomposite.h>
 
-typedef enum {STAGTEST,DATEST1,DATEST2} Test;
+/* Supply one of these with -test  */
+typedef enum {
+  STAGTEST=0,
+  DATEST1=1,
+  DATEST2=2
+} Test;
 
 int main(int argc,char **argv)
 {
@@ -41,29 +46,29 @@ int main(int argc,char **argv)
   // Note: we are only concerned with benchmarking well-balanced 3d problems for now
   ierr = PetscLogStagePush(creationStage);CHKERRQ(ierr);
   if (test == STAGTEST) {
-    ierr = DMStagCreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0,dof1,dof2,dof3,DMSTAG_GHOST_STENCIL_STAR,stencilWidth,NULL,NULL,NULL,&dm);CHKERRQ(ierr);
+    ierr = DMStagCreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0,dof1,dof2,dof3,DMSTAG_GHOST_STENCIL_BOX,stencilWidth,NULL,NULL,NULL,&dm);CHKERRQ(ierr);
   ierr = DMSetFromOptions(dm);CHKERRQ(ierr); // Use sparingly (for debugging)! Tests should compare the same functionality, remember.
   ierr = DMSetUp(dm);CHKERRQ(ierr);
   } else if (test == DATEST1) {
-    ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_STAR,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0+3*dof1+3*dof2+dof3,stencilWidth,NULL,NULL,NULL,&dm);CHKERRQ(ierr);
+    ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_BOX,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0+3*dof1+3*dof2+dof3,stencilWidth,NULL,NULL,NULL,&dm);CHKERRQ(ierr);
   ierr = DMSetFromOptions(dm);CHKERRQ(ierr); // Use sparingly (for debugging)! Tests should compare the same functionality, remember.
   ierr = DMSetUp(dm);CHKERRQ(ierr);
   } else if (test == DATEST2) {
     i = 0;
-    ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_STAR,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
+    ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_BOX,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
     ierr = DMSetFromOptions(dms[i]);CHKERRQ(ierr); // Use sparingly (for debugging)! Tests should compare the same functionality, remember.
     ierr = DMSetUp(dms[i]);CHKERRQ(ierr);
     for (i=1; i<4; ++i) {
-      ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_STAR,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof1,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
+      ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_BOX,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof1,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
       ierr = DMSetFromOptions(dms[i]);CHKERRQ(ierr); // Use sparingly (for debugging)! Tests should compare the same functionality, remember.
       ierr = DMSetUp(dms[i]);CHKERRQ(ierr);
     }
     for (; i<7; ++i) {
-      ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_STAR,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof2,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
+      ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_BOX,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof2,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
       ierr = DMSetFromOptions(dms[i]);CHKERRQ(ierr); // Use sparingly (for debugging)! Tests should compare the same functionality, remember.
       ierr = DMSetUp(dms[i]);CHKERRQ(ierr);
     }
-    ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_STAR,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
+    ierr = DMDACreate3d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DM_BOUNDARY_NONE,DMDA_STENCIL_BOX,Nx,Ny,Nz,PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,dof0,stencilWidth,NULL,NULL,NULL,&dms[i]);CHKERRQ(ierr);
     ierr = DMSetFromOptions(dms[i]);CHKERRQ(ierr); // Use sparingly (for debugging)! Tests should compare the same functionality, remember.
     ierr = DMSetUp(dms[i]);CHKERRQ(ierr);
     ierr = DMCompositeCreate(PETSC_COMM_WORLD,&dm);CHKERRQ(ierr);
