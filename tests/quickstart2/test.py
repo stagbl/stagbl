5ae0237cb3256cd3aa4092c648a74d209966b4ac
Patrick Sanan <patrick.sanan@gmail.com>
2019-03-29 17:47:51 -0700

    Quickstart: add 2d sinker, and add (vacuous) tests
    Add a second quickstart example.
    
    Add some tests (which check nothing) to make sure that the command
    lines in the README.md are run. These could be beefed up in the future,
    but this is just intended as a minimal check on staleness of the README.

---
+++ tests/quickstart2/test.py
@@ -0,0 +1,38 @@
+import os
+import pyTestHarness.test
+
+def test() :
+    # Note that we create this test in a special way, not with the usual
+    # use of create_stagbl_test(). Thus, do not use this as an example test file.
+
+    this_dir = os.path.split(os.path.abspath(__file__))[0]
+
+    STAGBL_DIR = os.getenv('STAGBL_DIR')
+    if not STAGBL_DIR :
+        raise RuntimeError('STAGBL_DIR not defined in environment')
+
+    STAGBL_ARCH = os.getenv('STAGBL_ARCH')
+    if not STAGBL_ARCH :
+        raise RuntimeError('STAGBL_ARCH not defined in environment')
+
+    command_details = "stagbldemo2d -pc_type lu -pc_factor_mat_solver_type mumps  -structure 2 -stag_grid_x 30 -stag_grid_y 50"
+
+    # Search the README.md to make sure the expected command is there
+    with open(os.path.join(STAGBL_DIR,'README.md')) as readme_file :
+        if not command_details in readme_file.read():
+            raise Exception("Did not find the expected quickstart command in README.md! : " + command)
+
+    command_full = os.path.join(STAGBL_DIR,STAGBL_ARCH,'bin',command_details)
+    print('debug',command_full)
+
+    t = pyTestHarness.test.Test("quickstart1",4,command_full,os.path.join(this_dir,'expected'))
+    t.set
+    t.setUseSandbox()
+
+    def comparefunc(t) :
+        # Check nothing
+        pass
+
+    t.setVerifyMethod(comparefunc)
+
+    return(t)
