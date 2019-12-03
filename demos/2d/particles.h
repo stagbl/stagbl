c2a7dcc4163af35660e632cc374c3c2897cf11f4
Patrick Sanan <patrick.sanan@gmail.com>
2019-12-03 10:49:43 +0100

    StagBLDemo2D: move particle creation out of main

--- demos/2d/particles.h
+++ demos/2d/particles.h
@@ -3,6 +3,7 @@
 
 #include "ctx.h"
 
+PetscErrorCode CreateParticleSystem(Ctx);
 PetscErrorCode InterpolateTemperatureToParticles(Ctx);
 PetscErrorCode MaterialPoint_AdvectRK1(Ctx,Vec,PetscReal);
 PetscErrorCode TestTeleport(Ctx);
