602047d587f2fab3316192922ac668265ac80f40
Patrick Sanan <patrick.sanan@gmail.com>
2019-12-10 11:39:46 -0800

    StagBLInitialize: accept MPI_COMM_NULL to use default comm

--- src/control/stagbl.c
+++ src/control/stagbl.c
@@ -5,7 +5,7 @@ PetscErrorCode StagBLInitialize(int argc,char** argv,const char *help,MPI_Comm c
 {
   PetscErrorCode ierr;
 
-  if (comm) PETSC_COMM_WORLD = comm;
+  if (comm && comm != MPI_COMM_NULL) PETSC_COMM_WORLD = comm;
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);CHKERRQ(ierr);
   return 0;
 }
