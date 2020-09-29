7f69c1635a85dda5307480528c46ffd9a4aecc24
Patrick Sanan <patrick.sanan@gmail.com>
2020-09-29 08:31:46 +0200

    Remove src/control and use src/stagbl.c

--- src/local.mk
+++ src/local.mk
@@ -2,4 +2,4 @@ libstagbl-y.c += $(call thisdir,\
   stagbl.c\
 )
 
-include $(call incsubdirs,control core stokes)
+include $(call incsubdirs,core stokes)
--- src/stagbl.c
+++ src/stagbl.c
@@ -1,7 +1,25 @@
 #include "stagbl.h"
 #include <string.h>
+#include <stdio.h>
 
 PetscBool StagBLCheckType(const char *type1, const char *type2)
 {
   return (PetscBool) strcmp(type1, type2) == 0;
+}
+
+PetscErrorCode StagBLInitialize(int argc,char** argv,const char *help,MPI_Comm comm)
+{
+  PetscErrorCode ierr;
+
+  if (comm && comm != MPI_COMM_NULL) PETSC_COMM_WORLD = comm;
+  ierr = PetscInitialize(&argc,&argv,(char*)0,help);CHKERRQ(ierr);
+  return 0;
+}
+
+PetscErrorCode StagBLFinalize()
+{
+  PetscErrorCode ierr;
+
+  ierr = PetscFinalize();
+  return ierr;
 }
